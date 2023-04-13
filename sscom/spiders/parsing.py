7d3b316917ddc89a23bedd50f0e98d3d74f89cbc
FR13NDS2020 <fr13nds2022@gmail.com>
2023-04-13 19:17:41 +0300

    added contacts parsing

--- sscom/spiders/parsing.py
+++ sscom/spiders/parsing.py
@@ -28,19 +28,20 @@ def get_item_links(response, seen_links=set()):
 
 
 def get_item_data(response):
-    """
-    Extracts data about a product from a web page.
+    # Extract contact information from the web page.
+    contacts_table = response.css('table.contacts_table tr')
+    contacts = {}
+    for contact in contacts_table:
+        name = contact.xpath('.//td[@class="ads_contacts_name"]/text()').get('')
+        value = contact.xpath('.//td[@class="ads_contacts"]//text()[normalize-space()]').getall()
+        if name and value:
+            # Check if the name already exists in the contacts dictionary, and append the new value.
+            if name in contacts:
+                contacts[name].append(''.join(value[:2]))
+            else:
+                contacts[name] = [''.join(value[:2])]
 
-    Args:
-        response: The response object returned by the web page.
-
-    Yields:
-        A dictionary containing the extracted data for each product.
-    """
-    # Extract the location of the product from the web page.
-    location = response.css('table.contacts_table tr:nth-child(4) td:nth-child(2)::text').get(default='')
-
-    # Extract the specifications of the product from the web page.
+    # Extract product specifications from the web page.
     specs_table = response.css('table.options_list tr')
     specs = {}
     for spec in specs_table:
@@ -49,32 +50,37 @@ def get_item_data(response):
         if spec_name and spec_value:
             specs[spec_name.strip()] = spec_value.strip()
 
-    # Extract the photos of the product from the web page.
+    # Extract car specifications from the web page.
+    cars_spec_table = response.css('div#msg_div_spec')
+    car_specs = cars_spec_table.css('b.auto_c::text').getall()
+
+    # Extract photo information from the web page.
     photo_label = response.css('div.ads_photo_label')
     photos = photo_label.css('img.pic_thumbnail.isfoto::attr(src)').getall()
 
     # Extract the price and date of the product from the web page.
-    # Use xpath to get the price, as css selector is not working properly
-    price = response.xpath('(//span[@class="ads_price"] | //td[@class="ads_price"])/text()').get(default='')
+    # Use xpath to get the price, as css selector is not working properly.
+    price = response.xpath('(//span[@class="ads_price"] | //td[@class="ads_price"])/text()').get(default='buy')
     date = response.css('td.msg_footer::text').get(default='')
 
     # Create a dictionary containing all the extracted data for the product.
     data = {
         'date': date.strip(),
         'price': price.strip(),
-        'location': location.strip(),
+        'contacts': contacts,
         'specs': specs,
+        'car_specs': car_specs,
         'photos': photos,
     }
 
-    # Yield the data for the product.
     yield data
+
 
 
 class ParsingSpider(scrapy.Spider):
     name = "parsing"
     allowed_domains = ["ss.com"]
-    start_urls = ["https://www.ss.com/msg/en/electronics/phones/mobile-phones/apple/iphone-14-pro-max/cbklco.html"]
+    start_urls = ["https://www.ss.com/msg/en/transport/cars/alfa-romeo/giulia/cxxong.html"]
     # start_urls = ["http://ss.com/en/electronics/"]
 
     def parse(self, response):
